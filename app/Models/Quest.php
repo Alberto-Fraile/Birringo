c8914ee774eabd8837e47e623f7be070e54b4c07
Jonace <jonathan_miguel_onrubia_tsapp1ta2021@cev.com>
2022-02-26 12:28:51 +0100

    quest model changes

--- app/Models/Quest.php
+++ app/Models/Quest.php
@@ -14,6 +14,6 @@ class Quest extends Model
         return $this->belongsToMany(User::class,'user_quests');
     }
     public function pub(){
-        return $this->belongsTo(Pub::class, 'id');
+        return $this->hasOne(Pub::class, 'id');
     }
 }
