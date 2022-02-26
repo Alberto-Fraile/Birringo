efc211c9e8d3ad9341d721024234d2905fb465c7
Jonace <jonathan_miguel_onrubia_tsapp1ta2021@cev.com>
2022-02-26 10:45:02 +0100

    fix error data  in quests 1.3

--- app/Http/Controllers/QuestController.php
+++ app/Http/Controllers/QuestController.php
@@ -18,8 +18,7 @@ class QuestController extends Controller
         $respuesta = ["status" => 1, "msg" => ""];
 
         try {
-            $quests = Quest::with('pub')
-            ->get();
+            $quests = $quests->pub();
             
             if ($quests){
                 $respuesta['msg'] = "Quests encontrados";
