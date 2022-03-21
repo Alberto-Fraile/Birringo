65e243aebda3df036f59677ffb9eaef5a019c0a5
Jonace <jonathan_miguel_onrubia_tsapp1ta2021@cev.com>
2022-03-21 12:41:37 +0100

    google login deleted "no https"

--- config/services.php
+++ config/services.php
@@ -29,13 +29,4 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-
-    'google' => [
-        //Id suministrado por google        
-        'client_id'     => '1065119762743-gbco8ifhqr8t5fo33qgmra2su6nqijt1.apps.googleusercontent.com', 
-        //Secret suministrado por google 
-        'client_secret' => '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',
-        //Página a la que sera redireccionado el navegador cuando el login se exitoso 
-        'redirect'      => 'http://localhost/ventaCartas/public/api/usuarios/google/callback'
-     ]
 ];
